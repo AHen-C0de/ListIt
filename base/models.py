e47b7e92511f401e092ad638b086a942f2a5261d
angela <angela.neuron@gmail.com>
2023-07-30 21:55:29 +0200

    create models and migrations

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class List(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+      return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    list = models.ForeignKey(
+        List,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
