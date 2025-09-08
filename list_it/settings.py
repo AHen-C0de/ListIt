6cd75b384067fca460254c63da221c42dd47bf87
Angie <angela.neuron@gmail.com>
2025-09-08 16:20:09 +0200

    Add allowed-hosts; add .env file

--- list_it/settings.py
+++ list_it/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+LOCAL_IP = os.getenv("LOCAL_IP")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +30,7 @@ SECRET_KEY = 'django-insecure-&yz9aa-@msn1e!#pwh0nzudah$_df_^^lr#62n$kzg$=^vg75$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost", LOCAL_IP]
 
 
 # Application definition
