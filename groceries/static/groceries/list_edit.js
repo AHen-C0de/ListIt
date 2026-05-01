3c181c2e0e2c9180859dcf5d49f0f8dcfa74e82b
quest.notfound404 <quest.notfound404@gmail.com>
2026-05-01 19:34:30 +0200

    add a search bar to the edit modal

--- groceries/static/groceries/list_edit.js
+++ groceries/static/groceries/list_edit.js
@@ -1,3 +1,7 @@
 function openListEdit() {
     $("#modal-background").css("display", "flex"); // TODO: .show()
+    let $template = $("#edit-template").contents();
+    $template.attr("id", "edit");
+    let $modal = $("#modal");
+    $template.appendTo($modal);
 }
