eefca8f9a92807b49961c10ec6450ec973d065a9
quest.notfound404 <quest.notfound404@gmail.com>
2026-05-02 23:04:38 +0200

    add comments

--- groceries/static/groceries/list_edit.js
+++ groceries/static/groceries/list_edit.js
@@ -1,17 +1,19 @@
 function openListEdit() {
+    // show modal
     $("#modal-background").css("display", "flex"); // TODO: .show()
+    // copy edit template into it
     let $template = $("#edit-template").contents();
     $template.attr("id", "edit");
-    let $modal = $("#modal");
-    $template.appendTo($modal);
-    $template.find("input").on("input", getGroceries)
+    $template.appendTo($("#modal"));
+    // attach onInput event handler to the input field
+    $template.find("input").on("input", getGroceries);
 }
 
 function getGroceries(e) {
-    let $input = $(e.target)
+    let $input = $(e.target);
     $.ajax({
         type: "GET",
         url: $input.attr("x-url"),
-        data: {search: e.target.value.trim()},
+        data: {search: $input.value.trim()}, //* param-name must align to expected one from SearchFilter class in viewset
     });
 }
